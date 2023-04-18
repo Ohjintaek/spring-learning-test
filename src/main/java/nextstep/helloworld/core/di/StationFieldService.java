7bb51fa99d0cb65e4c115fe19ce89755180f1b14
2014012606 <2014012606@hanyang.ac.kr>
2023-04-18 11:36:39 +0900

    feat: Core 연습

--- src/main/java/nextstep/helloworld/core/di/StationFieldService.java
+++ src/main/java/nextstep/helloworld/core/di/StationFieldService.java
@@ -1,7 +1,14 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationFieldService {
     private StationRepository stationRepository;
+
+    public StationFieldService() {
+        this.stationRepository = new StationRepository();
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
